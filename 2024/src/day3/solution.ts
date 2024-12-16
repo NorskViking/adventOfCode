ebdf9dd18324f61e7c704510f287392096c8bb9d
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-12-16 19:15:33 +0100

    :construction: Start part 2
    - Start solving part 2

--- 2024/src/day3/solution.ts
+++ 2024/src/day3/solution.ts
@@ -2,20 +2,34 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 // Read input file
-const inputPath = path.resolve(__dirname, 'input.txt');
+const inputPath = path.resolve(__dirname, 'test_input.txt');
 const input = fs.readFileSync(inputPath, 'utf8').trim();
 
 const regexOfValidMul = /mul\(\s*(\d+)\s*,\s*(\d+)\s*\)/g;
+const regexDo = /do()/;
+const regexDont = /don't()/
 
-const mulMatches = input.matchAll(regexOfValidMul);
 
-let totalSum: number = 0;
-
-for (const match of mulMatches) {
-    const firstValue: number = parseInt(match[1], 10);
-    const secoundValue: number = parseInt(match[2], 10);
-
-    totalSum += (firstValue * secoundValue);
+const solvePartOne = (corruptedText: string): number => {
+    let totalSum: number = 0;
+    
+    const mulMatches = corruptedText.matchAll(regexOfValidMul);
+    
+    for (const match of mulMatches) {
+        const firstValue: number = parseInt(match[1], 10);
+        const secoundValue: number = parseInt(match[2], 10);
+    
+        totalSum += (firstValue * secoundValue);
+    }
+    return totalSum;
 }
 
-console.log(totalSum);
+const solvePartTwo = (corruptedText: string): number => {
+    let totalSum: number = 0;
+
+    return totalSum;
+}
+
+
+console.log("Part one solution: " + solvePartOne(input));
+console.log("Part two solution: " + solvePartTwo(input));
