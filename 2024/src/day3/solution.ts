49c3afae4b1bbb1fee84550935c08e0a4dd1aa66
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-12-16 19:03:39 +0100

    :sparkles: Part 1 of day 3
    Part 1 of day 3 successfull.

--- 2024/src/day3/solution.ts
+++ 2024/src/day3/solution.ts
@@ -2,6 +2,20 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 // Read input file
-const inputPath = path.resolve(__dirname, 'test_input.txt');
+const inputPath = path.resolve(__dirname, 'input.txt');
 const input = fs.readFileSync(inputPath, 'utf8').trim();
 
+const regexOfValidMul = /mul\(\s*(\d+)\s*,\s*(\d+)\s*\)/g;
+
+const mulMatches = input.matchAll(regexOfValidMul);
+
+let totalSum: number = 0;
+
+for (const match of mulMatches) {
+    const firstValue: number = parseInt(match[1], 10);
+    const secoundValue: number = parseInt(match[2], 10);
+
+    totalSum += (firstValue * secoundValue);
+}
+
+console.log(totalSum);
