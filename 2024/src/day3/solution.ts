0518e4471dce0aeb0d19663dd8828f63c8e26607
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-12-17 12:13:31 +0100

    ✨ Solution day 3
    - Day 3 challenge part 1 and 2 complete.

--- 2024/src/day3/solution.ts
+++ 2024/src/day3/solution.ts
@@ -2,31 +2,47 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 // Read input file
-const inputPath = path.resolve(__dirname, 'test_input.txt');
+const inputPath = path.resolve(__dirname, 'input.txt');
 const input = fs.readFileSync(inputPath, 'utf8').trim();
-
-const regexOfValidMul = /mul\(\s*(\d+)\s*,\s*(\d+)\s*\)/g;
-const regexDo = /do()/;
-const regexDont = /don't()/
-
 
 const solvePartOne = (corruptedText: string): number => {
     let totalSum: number = 0;
+    const regex = /mul\(\s*(\d+)\s*,\s*(\d+)\s*\)/g; // Regex for finding instances of "mul(value,value)"
     
-    const mulMatches = corruptedText.matchAll(regexOfValidMul);
+    const mulMatches = corruptedText.matchAll(regex);
     
     for (const match of mulMatches) {
         const firstValue: number = parseInt(match[1], 10);
-        const secoundValue: number = parseInt(match[2], 10);
+        const secondValue: number = parseInt(match[2], 10);
     
-        totalSum += (firstValue * secoundValue);
+        totalSum += (firstValue * secondValue);
     }
     return totalSum;
 }
 
 const solvePartTwo = (corruptedText: string): number => {
-    let totalSum: number = 0;
+    let totalSum: number = 0; // Number of non-corrupted multiplications found
+    const regex = /do\(\)|don't\(\)|mul\(\s*(\d+)\s*,\s*(\d+)\s*\)/g; 
+    let readInput: boolean = true;
 
+    let match: RegExpExecArray | null;
+
+    while ((match = regex.exec(corruptedText)) !== null) {
+        const found = match[0];
+
+        if (found === "do()") {
+            // Start reading mul(val,val) again
+            readInput = true;
+        } else if (found === "don't()") {
+            readInput = false;
+        } else if (found.startsWith("mul(")) {
+            if (readInput) {
+                const firstValue: number = parseInt(match[1], 10);
+                const secondValue: number = parseInt(match[2], 10); 
+                totalSum += (firstValue * secondValue);
+            }
+        }
+    }
     return totalSum;
 }
 
