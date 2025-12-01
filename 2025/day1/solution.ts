fb11f45905205a90f87eb7435bf80f5677eaf3c4
Jørgen <70204936+NorskViking@users.noreply.github.com>
2025-12-01 17:10:17 +0100

    init: set up base structure for solution

---
+++ 2025/day1/solution.ts
@@ -0,0 +1,16 @@
+import { readInput } from "../../utils/helpers";
+import { readLines } from "../../utils/helpers";
+
+// Read input files
+const puzzle_data: string = readInput('./test_input.txt');
+const puzzle_lines: string[] = readLines(puzzle_data);
+
+const rotation_direction: string[] = [];
+const rotation_number: number[] = [];
+
+// Find direction and number
+for (const line of puzzle_data) {
+    
+}
+
+const solve_roation_puzzle = () => {};
