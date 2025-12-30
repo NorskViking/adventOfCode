70f197c2ca64a446f2111c29dd1337bb1d8b3e54
Jørgen <70204936+NorskViking@users.noreply.github.com>
2025-12-30 14:43:02 +0100

    feat: add helper functions
    Add helper functions, separating ID-ranges from IDs, for easier calculations later.

--- 2025/day5/solution.ts
+++ 2025/day5/solution.ts
@@ -6,3 +6,29 @@ const puzzleData: string = reader.readInput('test_input.txt');
 const puzzleLines: string[] = reader.readLines(puzzleData);
 
 console.log(puzzleLines);
+
+const idRanges: [number, number][] = [];
+const produceIDs: number[] = [];
+
+const rangeAndIdSeperatorIndex = puzzleLines.findIndex(line => line.trim() === '');
+
+/**
+ * Find all ID ranges for fresh produce
+ */
+for (let i = 0; i < rangeAndIdSeperatorIndex; i++) {
+    const [lowerRange, upperRange] = puzzleLines[i]?.split('-');
+    const lowerValue = parseInt(lowerRange);
+    const upperValue = parseInt(upperRange);
+    idRanges.push([lowerValue, upperValue])
+}
+
+/**
+ * Find all produce IDs
+ */
+for (let y = rangeAndIdSeperatorIndex; y < puzzleLines.length; y++) {
+    const ids = puzzleLines[y];
+    if (ids) {
+        produceIDs.push(parseInt(ids))
+    }
+}
+
