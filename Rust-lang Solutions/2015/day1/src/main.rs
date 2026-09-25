f4c9e5ea92e7f71ff97d81c119afbac136b58d9e
Jørgen <70204936+NorskViking@users.noreply.github.com>
2026-09-25 10:56:24 +0200

    :100: Day 1 part 1 and 2
    Solve 2015 day 1 puzzle part 1 and part 2

--- Rust-lang Solutions/2015/day1/src/main.rs
+++ Rust-lang Solutions/2015/day1/src/main.rs
@@ -1,10 +1,56 @@
 use std::fs;
 
 fn main() {
-    println!("2015, Day 1\n");
+    println!("Advent of Code 2015, Day 1\n");
 
-    let puzzle_input = fs::read_to_string("puzzle.txt")
+    let mut floor: i32 = 0;
+    let mut position = 0;
+
+    /* ------------
+        Part 1
+        -----------
+     */
+
+    let puzzle = fs::read_to_string("puzzle.txt")
         .expect("Should read puzzle file");
 
-    println!("{puzzle_input}");
+    //Starting at floor 0, move one floor up if char is '(' or down if ')'
+    for element in puzzle.chars() {
+        if element == '(' {
+            floor += 1;
+        } else if element == ')' {
+            floor -= 1;
+        } else {
+            println!("Something wrong happended with element: {element}\n");
+        }
+    }
+
+    println!("Santa have arrieved at floor: {floor}\n");
+
+    /* ------------
+        Part 2
+        -----------
+     */
+    //Find first occurrence of hitting floor -1, and return the position in the string
+    
+    floor = 0; //remember to reset floor count
+
+    for element in puzzle.chars() {
+        if element == '(' {
+            floor += 1;
+        } else if element == ')' {
+            floor -= 1;
+        } else {
+            println!("Something wrong happended with element: {element}\n");
+            continue;
+        }
+
+        position += 1;
+
+        if floor == -1 {
+            println!("Found the basement at position {position}");
+            break;
+        }
+    }
+
 }
